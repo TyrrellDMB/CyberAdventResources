d28aaf67096f57b5f972eade421362f6cc7e14c1
Tyrrell <Tbrewster0612@gmail.com>
2022-05-30 13:36:44 -0400

    gd

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ function updateVisitCount () {
   fetch('countURL')
     .then(res => res.json())
     .then(res => {
-      countElement.innerHTML = res.body
+      countElement.innerHTML = res.stat
     })
 }
