32b097030a27b5b4bad623cba660dd7ae08cacbe
Tyrrell <Tbrewster0612@gmail.com>
2022-05-30 00:23:03 -0400

    Cleaning Up Code For Linter

--- Count.js
+++ Count.js
@@ -1,11 +1,11 @@
-const countEL = document.getElementById('count');
+const countEL = document.getElementById('count')
 
-updateVisitCount();
+updateVisitCount()
 
-function updateVisitCount(){
-    fetch('https://api.countapi.xyz/update/cyberadvent.com/59422b92-b1e7-4352-a847-c73c1caf8b99/?amount=1')
-    .then(res => res.json())
-    .then(res => {
-        countEL.innerHTML = res.value;
+function updateVisitCount () {
+  fetch('https://api.countapi.xyz/update/cyberadvent.com/59422b92-b1e7-4352-a847-c73c1caf8b99/?amount=1')
+  .then(res => res.json())
+  .then(res => {
+      countEL.innerHTML = res.value
     })
 }
