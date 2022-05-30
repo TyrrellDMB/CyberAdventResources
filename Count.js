c767e50c112e71314126918138844cad0e301592
Tyrrell <Tbrewster0612@gmail.com>
2022-05-30 00:33:37 -0400

    Updated code to fit linters requirements

--- Count.js
+++ Count.js
@@ -6,6 +6,6 @@ function updateVisitCount () {
   fetch('https://api.countapi.xyz/update/cyberadvent.com/59422b92-b1e7-4352-a847-c73c1caf8b99/?amount=1')
     .then(res => res.json())
     .then(res => {
-        countEL.innerHTML = res.value
+      countEL.innerHTML = res.value
     })
 }
