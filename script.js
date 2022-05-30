c686501781fcbbbb8cfdaa23c26a2f1a77d525c4
Tyrrell <Tbrewster0612@gmail.com>
2022-05-30 15:25:52 -0400

    ugjv

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const countEL = document.getElementById('stat')
 updateVisitCount()
 
 function updateVisitCount () {
-  fetch('https://nni2vx3l2g.execute-api.us-east-1.amazonaws.com/default/updateViewcount')
+  fetch('https://api.countapi.xyz/update/cyberadvent.com/59422b92-b1e7-4352-a847-c73c1caf8b99/?amount=1')
     .then(res => res.json())
     .then(res => {
       countEL.innerHTML = res.value
