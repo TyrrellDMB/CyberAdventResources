92b367f36b0bf90d5ccd9522e00be23645018ba4
Tyrrell <Tbrewster0612@gmail.com>
2022-05-30 13:53:51 -0400

    sds

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const countURL = 'https://nni2vx3l2g.execute-api.us-east-1.amazonaws.com/default
 const countElement = document.getElementById('stat');
 
 
-updateVisitCount()
+updateVisitCount();
 
-function updateVisitCount () {
+function updateVisitCount() {
   fetch('countURL')
-    .then(res => res.json())
-    .then(res => {
-      countElement.innerHTML = res.quantity;
+  .then(res => res.json())
+  .then(res => {
+    countElement.innerHTML = res.quantity;
     })
 }
